2ac26e9e0a5e9aea50d3ef04effde57b4cf11f3d
Muhammad Fauzan <developerfauzan@gmail.com>
2020-11-11 17:38:47 +0700

    revert changes

--- src/server.js
+++ src/server.js
@@ -3,6 +3,12 @@ const app = express();
 const serverless = require('serverless-http');
 const path = require('path');
 
+app.get('/', (req, res) => {
+    res.send(`
+    This is billboard chart scraper open source API.
+    <a href="https://github.com/fauzan121002/billboard-chart-scraper" target="_blank">Click here for guide</a>
+    `);
+});
 app.use('/.netlify/functions/server',require('./router'));
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
