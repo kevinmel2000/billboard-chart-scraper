23a90152e69524e8b71cdb3a97f54f741752ff2a
Muhammad Fauzan <developerfauzan@gmail.com>
2020-11-11 15:55:37 +0700

    fix: delete exports app

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const serverless = require('serverless-http');
 const PORT = 3000;
 const routes = require('./src/router')
 
-app.use('/.netlify/functions/server',routes);
+app.use('/',routes);
 
-app.listen(PORT);
 console.log(`Server listening on port ${PORT}`);
 
-module.exports = app;
 module.exports.handler = serverless(app);
+
+app.listen(PORT);
