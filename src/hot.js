a20b0861baedb374618fdf626ffc1418944a951b
Muhammad Fauzan <developerfauzan@gmail.com>
2020-11-11 15:45:15 +0700

    chore(.gitignore): delete src

---
+++ src/hot.js
@@ -0,0 +1,34 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const express = require('express');
+const router = express.Router();
+
+router.get('/hot-100', (req, res) => {
+  url = 'http://www.billboard.com/charts/hot-100';
+
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
+      let chartList = [];
+
+      $('.chart-list__elements li').each(function(i,elm){
+        let data = $(elm);
+        let proxyObject = {};
+
+        proxyObject.rank = data.children().children().children().html();
+        proxyObject.song = data.children().find('.chart-element__information').find(".chart-element__information__song").html();
+        proxyObject.artist = data.children().find('.chart-element__information').find(".chart-element__information__artist").html();
+        proxyObject.delta = data.children().find('.chart-element__information').find(".chart-element__information__delta").find(".chart-element__information__delta__text").html();
+      
+        chartList.push(proxyObject);
+      });
+
+      res.status(200).json({
+        message: 'success',
+        data: chartList
+      });
+    }
+  });
+});
+
+module.exports = router;
---
+++ src/router.js
@@ -0,0 +1,4 @@
+module.exports = require('./index');
+module.exports = require('./billboard-global');
+module.exports = require('./billboard');
+module.exports = require('./hot');
